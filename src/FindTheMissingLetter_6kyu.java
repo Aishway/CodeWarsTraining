f3db3514cc6fce10430501478098b0a9e2fb37a9
Lenka Michalovicova <michalovicovalenka@gmail.com>
2022-02-01 20:46:36 +0100

    training

---
+++ src/FindTheMissingLetter_6kyu.java
@@ -0,0 +1,40 @@
+public class FindTheMissingLetter_6kyu {
+//    #Find the missing letter
+//
+//Write a method that takes an array of consecutive (increasing) letters as input and that returns the
+// missing letter in the array.
+//
+//You will always get an valid array. And it will be always exactly one letter be missing.
+// The length of the array will always be at least 2.
+//The array will always contain letters in only one case.
+//
+//Example:
+//
+//['a','b','c','d','f'] -> 'e' ['O','Q','R','S'] -> 'P'
+//
+//["a","b","c","d","f"] -> "e"
+//["O","Q","R","S"] -> "P"
+//(Use the English alphabet with 26 letters!)
+//
+//Have fun coding it and please don't forget to vote and rank this kata! :-)
+//
+//I have also created other katas. Take a look if you enjoyed this kata!
+
+    public static void main(String[] args) {
+        System.out.println(findMissingLetter(new char[] { 'a','b','c','d','f' }));
+    }
+
+    public static char findMissingLetter(char[] array) {
+        String alphabeth = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        int i = alphabeth.indexOf(array[0]);
+        int count = 0;
+
+        for (int j = i; j < alphabeth.length(); j++){
+            if (alphabeth.charAt(j) != array[count]){
+                return alphabeth.charAt(j);
+            }
+            count++;
+        }
+        return ' ';
+    }
+}
